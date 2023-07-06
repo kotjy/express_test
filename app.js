0e5065aa2e62bcdc936fb23a246fe5d773159c56
jendu <maggie891209@gmail.com>
2023-07-06 14:19:12 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const exphbs = require('express-handlebars')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+  res.render("index", { style: "home.css" });
+});
+
+app.get("/about", (req, res) => {
+  res.render("About", { style: "about.css" });
+});
+
+app.get("/portfolio", (req, res) => {
+  res.render("portfolio", { style: "portfolio.css" });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { style: "contact.css" });
+});
+
+
+
+
+app.listen(port,() =>{
+  console.log(`Express is listening on localhost:${port}`)
+})
+
